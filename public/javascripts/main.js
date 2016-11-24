6539802de28f6648ae840480417a914e696c9213
Jullia <jullia.pimenta02@gmail.com>
2016-11-23 23:54:40 -0200

    criando script  para pegar dados do  /fazenda.json

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,22 @@
 // your code
+function teste(evento){
+  if (evento.target.readyState === 4) {
+      var ranking  =  JSON.parse(evento.target.responseText);
+      names(ranking.data);
+  }
+
+}
+
+var httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = teste;
+httpRequest.open('GET', '/fazenda.json', true);
+httpRequest.send();
+
+function names(participants) {
+  for (var i = 0; i < participants.length; i++) {
+    var participant = participants[i];
+    console.log(participant);
+
+  }
+
+}
